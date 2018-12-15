5f8c11f32775ee077b237e2c79fef66a229d0897
Denys <denis.varlan95@gmail.com>
2018-12-16 00:19:24 +0100

    Work with massive

--- src/app/cars/cars.component.ts
+++ src/app/cars/cars.component.ts
@@ -9,16 +9,24 @@ import { TargetLocator } from 'selenium-webdriver';
 })
 export class CarsComponent {
 
-     AddCars = '';
-     inputText = '';
+     CarName = '';
+     CarStatus = false;
+     cars = ['Ford', 'Audi', 'BMW', 'Mazda', 'Bentli', 'Lada'];
+     dates = [
+       new Date(2025, 3, 4),
+       new Date(2024, 4, 4),
+       new Date(2023, 5, 4),
+       new Date(2022, 6, 4),
+       new Date(2021, 7, 4),
+       new Date(2020, 8, 4)
+     ];
       constructor () {
 
       }
       addCar() {
-        this.AddCars = 'Added Car';
+        this.CarStatus = true;
+        this.cars.push(this.CarName);
+        this.CarName = '';
       }
-  onKeyUp(event) {
-  this.inputText = event.target.value;
-    }
 }
 
